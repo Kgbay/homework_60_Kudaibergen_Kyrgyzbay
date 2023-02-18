fd1b70a4e5db64581bab04a876f13e351dfc75e8
Kudaibergen Kyrgyzbay <kyrgyzbay.kudaibergen@gmail.com>
2023-02-18 10:19:30 +0600

    Создал сущность Товары - Product

---
+++ online_store/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Наименование')
+    desc = models.TextField(max_length=3000, null=True, verbose_name='Описание', default='Описание товара')
+    category_fk = models.IntegerField(max_length=20, verbose_name='id категорий')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата и время обновления')
+    price = models.DecimalField(max_length=20, verbose_name='Цена')
+    img_link = models.TextField(max_length=3000, verbose_name='Ссылка на изображения')
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
