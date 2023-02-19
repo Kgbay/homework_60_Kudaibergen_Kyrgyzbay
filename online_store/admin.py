9c7d9a3fbbf32ba7448e11e6cf6c1f2381fc204b
Kudaibergen Kyrgyzbay <kyrgyzbay.kudaibergen@gmail.com>
2023-02-19 09:25:38 +0600

    Добавил представления для admin панели

--- online_store/admin.py
+++ online_store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Product
+
+
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price')
+    list_filter = ('id', 'name', 'price', 'desc')
+    search_fields = ('name', 'price')
+    fields = ('name', 'desc', 'category_id', 'price')
+    readonly_fields = ('id', 'created_at', 'updated_at')
+
+admin.site.register(Product, ProductAdmin)
